1127fcfa5e01005af215df98995ad3db3f5c7712
BenasPalivonas <69222998+BenasPalivonas@users.noreply.github.com>
2021-02-24 13:10:15 +0200

    adding package.json

--- server.js
+++ server.js
@@ -7,3 +7,6 @@ app.use(bodyParser.json());
 app.listen(process.env.PORT, () => {
     console.log("app is running on port ", process.env.PORT);
 })
+app.get('/', (req, res) => {
+    res.json("its working")
+})
