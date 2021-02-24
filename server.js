08f51fc865b2278b70d76d831661f82930e3cf05
BenasPalivonas <69222998+BenasPalivonas@users.noreply.github.com>
2021-02-24 12:48:35 +0200

    commiting

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.listen(process.env.PORT, () => {
+    console.log("app is running on port ", process.env.PORT);
+})
