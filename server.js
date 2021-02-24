ff94967680d9abd2f8071905cbca1fd10662e191
BenasPalivonas <69222998+BenasPalivonas@users.noreply.github.com>
2021-02-24 13:29:45 +0200

    commiting

--- server.js
+++ server.js
@@ -17,16 +17,16 @@ app.listen(process.env.PORT, () => {
     console.log("app is running on port ", process.env.PORT);
 })
 app.get('/', (req, res) => {
-    res.json("its working")
+    res.json("its working");
 })
 app.get('/top3', () => {
-
+    res.json("its working");
 });
-app.post('/addToLeaderBoard', () => {
-    const { name, score } = req.body;
-    console.log(req.body);
-    knex('players').insert({
-        name: name,
-        score: score
-    }).then(res.json("registered"));
-});
+// app.post('/addToLeaderBoard', () => {
+//     const { name, score } = req.body;
+//     console.log(req.body);
+//     knex('players').insert({
+//         name: name,
+//         score: score
+//     }).then(res.json("registered"));
+// });
