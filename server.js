34c7f29f172549db6c92cbb30cdd5ee11ddc92b1
BenasPalivonas <69222998+BenasPalivonas@users.noreply.github.com>
2021-02-24 13:36:07 +0200

    commiting

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const cors = require('cors');
 app.use(cors());
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-// const knex = require('knex')({
-//     client: 'pg',
-//     connection: {
-//         connectionString: process.env.DATABASE_URL,
-//         ssl: {
-//             rejectUnauthorized: false
-//         }
-//     }
-// });
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
 app.listen(process.env.PORT, () => {
     console.log("app is running on port ", process.env.PORT);
 })
@@ -22,11 +22,11 @@ app.get('/', (req, res) => {
 app.get('/top3', () => {
     res.json("its working");
 });
-// app.post('/addToLeaderBoard', () => {
-//     const { name, score } = req.body;
-//     console.log(req.body);
-//     knex('players').insert({
-//         name: name,
-//         score: score
-//     }).then(res.json("registered"));
-// });
+app.post('/addToLeaderBoard', () => {
+    const { name, score } = req.body;
+    console.log(req.body);
+    knex('players').insert({
+        name: name,
+        score: score
+    }).then(res.json("registered"));
+});
